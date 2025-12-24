38f11c01a10e2998be8c4f7748c0184236617aba
Victoria Joh <johvj@victoriasm4mbp.lan>
2025-12-23 16:29:28 -0800

    add link router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './main';
+import { LinkTree } from './pages/linktree';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<LinkTree/>}/>
+        {/* <Route path="portfolio" element={<Portfolio/>} /> */}
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/pages/linktree.js
+++ src/pages/linktree.js
@@ -1,7 +1,7 @@
 import './styles.css';
-import {Link} from './components/link/index.tsx';
+import { Link } from './link/index.tsx';
 
-function App() {
+export const LinkTree = () => {
   return (
     <div className="App">
       <div className="cover">
@@ -10,7 +10,7 @@ function App() {
       <header className="header">
         <div id="links">
           <Link color="#285E6D" title="BLUESKY" icon="./icons/bluesky.svg" linkTo="https://bsky.app/profile/kvebox.bsky.social"/>
-          <Link color="#55969B" title="STORE" icon="./icons/instagram.svg" linkTo="https://www.inprnt.com/gallery/kveshoppe"/>
+          <Link color="#55969B" title="STORE" icon="./icons/inprnt.svg" linkTo="https://www.inprnt.com/gallery/kveshoppe"/>
           <Link color="#8FBB88" title="EMAIL" icon="./icons/instagram.svg" linkTo="https://www.github.com/kvebox"/>
           <Link color="#224B5E" title="PORTFOLIO"icon="./icons/instagram.svg" linkTo="/"/>
           <Link color="#449385" title="INSTAGRAM" icon="./icons/instagram.svg" linkTo="https://www.instagram.com/kvebox"/>
@@ -21,4 +21,3 @@ function App() {
   );
 }
 
-export default App;
